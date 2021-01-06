2d9116d460ab185d8293edd16973fbe8eb0df976
Eli Halych <eli.halych@gmail.com>
2021-01-06 17:39:16 +0100

    Remove unnecessary files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,7 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link> {/*Keep fore reference for now. TODO remove before merge 1-site-structure. */}
-  </Layout>
+  <h1>Hi there!</h1>
 )
 
 export default IndexPage
